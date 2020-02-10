fabb67bd288cf50a83c9524f4aefa561e7e360b7
SeungJun <sjc0829@naver.com>
2020-02-10 17:28:21 +0900

    주소문자열 URL 객체로 변환

--- test1.js
+++ test1.js
@@ -4,3 +4,9 @@ console.log('배열 요소의 수 : %d', Users.length);
 for(var i = 0; i < Users.length; i++) {
     console.log('배열 요소 #' + i + ' : %s', Users[i].name);
 }
+
+
+console.log('\n forEach 구문 사용하기');
+Users.forEach(function (item, index) {
+    console.log('배열 요소 #' + index + '%s', item.name);
+});
