61824863c4be433709a2aa34ebe243feee413fc8
SeungJun <sjc0829@naver.com>
2020-02-13 16:01:37 +0900

    파일 만들고 열고 닫고 쓰기(수정)

--- test11.js
+++ test11.js
@@ -4,12 +4,12 @@ fs.open('./output.txt', 'w', function(err, fd) {
     if(err) throw err;
 
     var buf = new Buffer('안녕!\n');
-    fs.write(fd, buf.length, null, function(err, written, buffer) {
+    fs.write(fd, buf, 0, buf.length, null, function(err, written, buffer) {
         if(err) throw err;
         console.log(err, written, buffer);
 
         fs.close(fd, function() {
-            console.log('파일 열고 데이터 쓰고 파일 닫기 완료.');
+        console.log('파일 열고 데이터 쓰고 파일 닫기 완료.');
        });
     });
 });
